5d684fc6120c6f13fd1b39f0840c8a274720fc80
Félix Laplante <75139846+felixlaplante@users.noreply.github.com>
2025-06-21 20:00:39 +0200

    Update __init__.py

--- uniform_bands/__init__.py
+++ uniform_bands/__init__.py
@@ -13,19 +13,19 @@ def _uniform(F_lo, F_hi, alpha):
     return {"lower": np.sort(F_lo, axis=0)[q_lo], "upper": np.sort(F_hi, axis=0)[q_hi]}
 
 
-def _student(F_lo, F_hi, alpha, eps):
+def _student(F_lo, F_hi, alpha, eps, min_val, max_val):
     mean_lo, mean_hi = F_lo.mean(axis=0), F_hi.mean(axis=0)
     std_lo, std_hi = F_lo.std(axis=0) + eps, F_hi.std(axis=0)
     T_lo, T_hi = (F_lo - mean_lo) / std_lo, (F_hi - mean_hi) / std_hi
     infT, supT = T_lo.min(axis=1), T_hi.max(axis=1)
     q_lo, q_hi = np.quantile(infT, alpha / 2), np.quantile(supT, 1 - alpha / 2)
     return {
-        "lower": np.clip(mean_lo + q_lo * std_lo, 0, 1),
-        "upper": np.clip(mean_hi + q_hi * std_hi, 0, 1),
+        "lower": np.clip(mean_lo + q_lo * std_lo, min_val, max_val),
+        "upper": np.clip(mean_hi + q_hi * std_hi, min_val, max_val),
     }
 
 
-def get_bands(F_lo, F_hi, alpha=0.05, eps=1e-8, method="uniform"):
+def get_bands(F_lo, F_hi, alpha=0.05, eps=1e-8, method="uniform", min_val=0.0, max_val=1.0):
     """
     Compute simultaneous confidence bands for a family of cumulative distribution functions (CDFs).
 
@@ -48,6 +48,12 @@ def get_bands(F_lo, F_hi, alpha=0.05, eps=1e-8, method="uniform"):
         Method used to compute the bands:
         - 'uniform': uses rank statistics to construct a uniform confidence envelope.
         - 'student': uses t-statistics (mean ± quantile × std) for marginal band approximation.
+
+    min_val : float, default=0.0
+        Minimum value of the functions.
+
+    max_val : float, default=1.0
+        Maximum value of the functions.
 
     Returns
     -------
@@ -74,4 +80,4 @@ def get_bands(F_lo, F_hi, alpha=0.05, eps=1e-8, method="uniform"):
 
     assert eps > 0
 
-    return _student(F_lo, F_hi, alpha, eps)
+    return _student(F_lo, F_hi, alpha, eps, min_val, max_val)
