55e892f7567b1df975c592cb65ed44dd9c962bd7
Félix Laplante <75139846+felixlaplante@users.noreply.github.com>
2025-07-24 23:34:46 +0200

    Add files via upload

--- uniformbands/interface.py
+++ uniformbands/interface.py
@@ -6,7 +6,7 @@ from ._methods import *
 
 def get_bands(
     F_lo: NDArray[np.float32 | np.float64],
-    F_hi: NDArray[np.float32 | np.float64],
+    F_hi: NDArray[np.float32 | np.float64] | None = None,
     alpha: float = 0.05,
     *,
     eps: float = 1e-8,
@@ -14,6 +14,29 @@ def get_bands(
     min_val: float = 0.0,
     max_val: float = 1.0,
 ) -> dict[str, NDArray[np.float32 | np.float64]]:
+    """Gets the uniform bands according the specified method.
+
+    Args:
+        F_lo (NDArray[np.float32  |  np.float64]): The lower high probability bounds.
+        F_hi (NDArray[np.float32  |  np.float64]):  The upper high probability bounds. Defaults to None.
+        alpha (float, optional): The level of supplementary risk. Defaults to 0.05.
+        eps (float, optional): The regularization parameter to ensure a well defined division.. Defaults to 1e-8.
+        method (str, optional): Either "uniform" or "student", the method used to compute uniform bands. Defaults to "uniform".
+        min_val (float, optional): The minimum accepted values of the functions.. Defaults to 0.0.
+        max_val (float, optional): The maximum accepted values of the functions.. Defaults to 1.0.
+
+    Raises:
+        ValueError: _description_
+        ValueError: _description_
+        ValueError: _description_
+        ValueError: _description_
+        ValueError: _description_
+
+    Returns:
+        dict[str, NDArray[np.float32 | np.float64]]: _description_
+    """
+
+    F_hi = F_hi if F_hi is not None else F_lo
 
     if F_lo.shape != F_hi.shape:
         raise ValueError(
